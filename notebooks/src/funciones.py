83caddd758f8f50704dec86304422d1fbf620ef1
Joaco <zaragozahoako10@gmail.com>
2025-03-29 20:08:55 -0300

    creación de funcion inicializadora para carga de valores de ranking final

--- notebooks/src/funciones.py
+++ notebooks/src/funciones.py
@@ -6,7 +6,12 @@ def orden_de_jugadores(round):
     dicc = {}
     for player, stats in round.items():
         dicc[player] = calcular_puntos(stats.values())
-    dicc = dict(sorted(dicc.items(),key = lambda item: item[1], reverse=True ))
-    return dicc
+    dicc_sorted = dict(sorted(dicc.items(),key = lambda item: item[1], reverse=True ))
+    return dicc_sorted
 
-    
+def inicializar_valores(round):
+    dicc_inicializado={}
+    for player in round.keys():
+        if player not in dicc_inicializado:
+            dicc_inicializado[player] = {"kills": 0,"assists": 0,"deaths": 0,"MVP":0,"points": 0}
+    return dicc_inicializado
