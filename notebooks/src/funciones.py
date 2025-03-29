d5519d325e1f4323985ac598b0edbe9dae86d205
Joaco <zaragozahoako10@gmail.com>
2025-03-29 18:34:07 -0300

    creación y uso de funcion de ordenamiento de jugadores + print basico de rondas

--- notebooks/src/funciones.py
+++ notebooks/src/funciones.py
@@ -1,6 +1,12 @@
 def calcular_puntos(stats):
-    kills, assists, deaths = stats.value()
+    kills, assists, deaths = stats
     return kills * 3 + assists + 1 if deaths else 0
 
+def orden_de_jugadores(round):
+    dicc = {}
+    for player, stats in round.items():
+        dicc[player] = calcular_puntos(stats.values())
+    dicc = dict(sorted(dicc.items(),key = lambda item: item[1], reverse=True ))
+    return dicc
 
-
+    
